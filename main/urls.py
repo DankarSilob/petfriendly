2c8ad8623a3f493528f2dca9d899211be0931f27
Carlos Manuel Chee Vázquez <carlosbronson@gmail.com>
2013-03-12 13:25:56 -0700

    Nuevas urls

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from main import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<id_lugar>\d+)/$', views.detail, name='detail'),
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<id_lugar>\d+)/$', views.detail, name='detail'),
+	url(r'^about/',TemplateView.as_view(template_name='main/about.html')),
+	url(r'^help/',TemplateView.as_view(template_name='main/help.html'))
 )
